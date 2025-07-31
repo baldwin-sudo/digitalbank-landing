d6e8eac934f3cd3b50a676f7825c4e74cc656703
baldwin <baldwin@192.168.1.189>
2025-07-31 19:56:43 +0100

    fxed images imports

---
+++ src/Hero.jsx
@@ -0,0 +1,35 @@
+import bg from "./images/bg-intro-desktop.svg";
+import mockup from "./images/image-mockups.png";
+
+export default function Hero() {
+  return (
+    <div className="flex flex-col-reverse lg:flex-row">
+      {/* left */}
+      <div className="flex flex-col items-start justify-center xl:justify-start mt-10 xl:mt-40 lg:items-start gap-10 w-full xl:w-1/2 px-10 lg:p-20 lg:px-30 xl:px-40">
+        <h1 className="text-5xl text-neutral-800 text-wrap">
+          Next generation digital banking
+        </h1>
+        <p className="text-neutral-400">
+          Take your financial life online. Your Digitalbank account will be a
+          one-stop-shop for spending, saving, budgeting, investing, and much
+          more.
+        </p>
+        <button className="self-center lg:self-start bg-gradient-to-r from-green-400 to-blue-400 rounded-full text-white px-8 py-3">
+          Request invite
+        </button>
+      </div>
+
+      {/* right */}
+      <div
+        style={{
+          backgroundImage: `url(${bg})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        }}
+        className="w-full flex justify-center items-center"
+      >
+        <img src={mockup} className="w-full max-w-[600px]" alt="App mockups" />
+      </div>
+    </div>
+  );
+}
